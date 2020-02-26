19f025160a2ac89f2d62932a20cf1ce70637e206
AnaLuizaMendes <analuhcm@gmail.com>
2020-02-26 13:58:47 +0000

    Last Updates

--- Lesson4_Part2.py
+++ Lesson4_Part2.py
@@ -21,3 +21,33 @@ for headline in headlines:
 
 print(news_ticker)
 print(len(news_ticker))
+
+## Your code should check if each number in the list is a prime number
+check_prime = [26, 39, 51, 53, 57, 79, 85]
+
+for number in check_prime:
+    i = 2
+    while i < number:
+        if number % i == 0:
+            print("{} is not a prime number, because {} is a factor of {}".format(number, i, number))
+            break
+        if i == (number - 1):
+            print('{} is not prime'.format(number))
+        i += 1
+
+## Zip exercise
+x_coord = [23, 53, 2, -12, 95, 103, 14, -5]
+y_coord = [677, 233, 405, 433, 905, 376, 432, 445]
+z_coord = [4, 16, -6, -42, 3, -6, 23, -1]
+labels = ["F", "J", "A", "Q", "Y", "B", "W", "X"]
+
+points = []
+for point in zip(labels, x_coord, y_coord, z_coord):
+    points.append("{}: {}, {}, {}".format(*point))
+
+for point in points:
+    print(point)
+
+
+
+
