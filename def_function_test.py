e553ef6eadab50327cb6ebbc66c624a9c3dd7ed4
AnaLuizaMendes <analuhcm@gmail.com>
2020-03-26 17:10:23 +0000

    update

--- def_function_test.py
+++ def_function_test.py
@@ -1,5 +1,6 @@
 # Learning how to make a def function
 
+
 def readable_time(days):
     # getting the integer that represents the number of weeks
     weeks = days // 7
@@ -8,12 +9,25 @@ def readable_time(days):
     string = '{} week(s) and {} day(s).'.format(weeks, rem_days)
     return string
 
+
 print(readable_time(40))
+
 
 def population_density(population, land_area):
     pop_density = population / land_area
     return pop_density
 
+
 test1 = population_density(20, 4)
 print("Result: {}".format(test1))
 
+counter = 0
+
+
+def increment(number):
+    number += 1
+    return number
+
+
+counter = increment(counter)
+print(counter)
