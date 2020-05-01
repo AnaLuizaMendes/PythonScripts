1748aecf39f0085d5df17871eb1e9863da5eec3a
AnaLuizaMendes <analuiza.cmendes@gmail.com>
2020-05-01 16:16:44 +0100

    Last Updates - Separating the Repositories in GitHub

--- For Loop Testing.py
+++
@@ -1,4 +0,0 @@
-names = ['Rob', 'Ana', 'Ritchie', 'Leona', 'David', 'Luciana', 'Denise']
-
-for x in names:
-    print(x.lower())
--- Practising Basic Challenges.py
+++
@@ -1,6 +0,0 @@
-#print problem HackerRank
-# how to print a  range without space between the numbers
-n = int(input())
-
-for n in range(1, n + 1):
-    print(n, end="")
